bfd531bfbaf68d25e62de5d4ca46ce2849e85b0c
Miika <123482448+theMsH@users.noreply.github.com>
2025-02-22 23:46:08 +0200

    Add styling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,68 @@
+import pathlib
 import streamlit as st
 from model import train_model
 
 model = train_model()
 
-st.header("Sentiment analysis Streamlit App")
+# Load styles
+def load_css(file_path):
+    with open(file_path) as f:
+        st.html(f"<style>{f.read()}</style>")
+
+load_css(pathlib.Path("assets/styles.css"))
+
+
+# State params
+if 'required_message' not in st.session_state:
+    st.session_state.required_message = ""
+if 'sent_message' not in st.session_state:
+    st.session_state.sent_message = ""
+if 'result_message' not in st.session_state:
+    st.session_state.result_message = "Waiting your message"
+if 'result_icon' not in st.session_state:
+    st.session_state.result_icon = ':material/sentiment_content:'
+if 'submitted' not in st.session_state:
+    st.session_state.submitted = False
+
+
+st.title("Sentiment analysis Streamlit App")
+
+
+# Result icon
+st.write('# ' + st.session_state.result_icon)
+# Result message
+st.html('<h1 class="result-message">'+ st.session_state.result_message +'</h1>')
+# Sent message
+if st.session_state.sent_message == "":
+    st.write("")
+else:
+    st.write('"'+st.session_state.sent_message+'"')
+
+# Callback fuction
+def on_submit():
+    if st.session_state.form_message == "":
+        st.session_state.required_message = ":orange[This field is required!]"
+    else: 
+        st.session_state.sent_message = st.session_state.form_message
+        st.session_state.form_message = ""
+        st.session_state.required_message = ""
+
+        sentiment = model.predict([st.session_state.sent_message])[0]
+        st.session_state.result_message = f"Your message is {sentiment}!"
+        if sentiment == "positive":
+            st.session_state.result_icon = ":material/sentiment_satisfied:"
+        elif sentiment == "neutral":
+            st.session_state.result_icon = ":material/sentiment_neutral:"
+        elif sentiment == "negative":
+            st.session_state.result_icon = ":material/sentiment_dissatisfied:"
+
 
 with st.form("my_form"):
-    message = st.text_input(
-        "",
+    st.text_area(
+        label="Message",
+        label_visibility="collapsed",
         placeholder="Write something for me to analyze!",
+        key="form_message"
     )
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        sentiment = model.predict([message])
-        st.write("Result: ", sentiment[0])
-
-
-
+    st.write(st.session_state.required_message)
+    st.session_state.submitted = st.form_submit_button("Get sentiment!", on_click=on_submit)
